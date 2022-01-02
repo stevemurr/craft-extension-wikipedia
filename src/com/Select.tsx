d9f5c1a7112d6f7b60f92f16bcd112778fbe87aa
Steven Murr <stevemurr@gmail.com>
2022-01-02 13:49:22 -0800

    full width select

--- src/com/Select.tsx
+++ src/com/Select.tsx
@@ -2,7 +2,7 @@
 import * as React from "react"
 
 export const Select = (props: any) => {
-    return <select value={props.value} className="appearance-none" onChange={props.onChange}>
+    return <select style={{ width: "95%" }} className="appearance-none w-full px-3 py-2 mx-2 my-2" value={props.value} onChange={props.onChange}>
         {props.values.map((el: any, idx: any) => {
             return <option key={`select-${idx}`}>{el.name}</option>
         })}
